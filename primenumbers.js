836b6970ea674a80268940b6b7d8be1e1dedb99a
sai <saichowdary.7@gmail.com>
2021-09-02 02:36:50 -0400

    format

--- primenumbers.js
+++ primenumbers.js
@@ -1,30 +1,30 @@
 
 //Print Primenos in a given range
 
-function primeNums(size){
+function primeNums(size) {
 
     const arr = new Array(size);
 
-    let primeArr=[2]
-for(let n=3; n<arr.length; n+=2){
-    if(isPrime(n)){
-primeArr.push(n);
-    }
+    let primeArr = [2]
+    for (let n = 3; n < arr.length; n += 2) {
+        if (isPrime(n)) {
+            primeArr.push(n);
+        }
 
+    }
+    console.log(primeArr)
 }
-console.log(primeArr)
-}
-function isPrime(n){
-    let oddCtr=0;
-    for(let el=2; el < arr.length; el++){
-        if(n % el === 0 && el !== n){
+function isPrime(n) {
+    let oddCtr = 0;
+    for (let el = 2; el < arr.length; el++) {
+        if (n % el === 0 && el !== n) {
             oddCtr++
-            return 
+            return
         }
     }
-    if(oddCtr){
+    if (oddCtr) {
         return false;
-    }else{
+    } else {
         return true;
     }
 }
