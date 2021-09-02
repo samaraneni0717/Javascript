74181a9fccba06ad65b12c4041c1cc4878ed03e3
sai <saichowdary.7@gmail.com>
2021-09-02 04:09:19 -0400

    prime numbers--- es6 way

--- primenumbers.js
+++ primenumbers.js
@@ -1,32 +1,38 @@
 
 //Print Primenos in a given range
+//Complexity: O(n2)
+class PrimeNumbers {
 
-function primeNums(size) {
+    arr = [];
+    constructor(size) {
+        this.arr = new Array(size);
+        this.primeNums()
+    }
+    primeNums() {
+        let primeArr = [2]
+        for (let n = 3; n < this.arr.length; n += 2) {
+            if (this.isPrime(n)) {
+                primeArr.push(n);
+            }
 
-    const arr = new Array(size);
-
-    let primeArr = [2]
-    for (let n = 3; n < arr.length; n += 2) {
-        if (isPrime(n)) {
-            primeArr.push(n);
         }
-
+        console.log(primeArr)
     }
-    console.log(primeArr)
-}
-function isPrime(n) {
-    let oddCtr = 0;
-    for (let el = 2; el < arr.length; el++) {
-        if (n % el === 0 && el !== n) {
-            oddCtr++
-            return
+    isPrime(n) {
+        let oddCtr = 0;
+        for (let el = 2; el < this.arr.length; el++) {
+            if (n % el === 0 && el !== n) {
+                oddCtr++
+                return
+            }
+        }
+        if (oddCtr) {
+            return false;
+        } else {
+            return true;
         }
     }
-    if (oddCtr) {
-        return false;
-    } else {
-        return true;
-    }
 }
 
-console.log(primeNums())
+
+console.log(new PrimeNumbers(100));
