e28020e00d5007cc933d0da39f2cda7062e4b75e
sai <saichowdary.7@gmail.com>
2021-09-05 14:43:55 -0400

    reverse num

--- reverseanumber.js
+++ reverseanumber.js
@@ -1,21 +1,49 @@
-function reverseNum(num){
+function reverseNum(num) {
 
-//132= 100+30+2
-//231 = 
+    //132= 100+30+2
+    //231 = 
 
-// 132 % 10 =2
-// 132/10 =13
-// 13 % 10 = 3
-// 13/10 =1
-// 1%10 = 1
-// 1/10 =
-let revNum =0;
-while(num > 0){
-let val =num % 10;
-num = Math.floor(num/10);
-revNum = revNum * 10 + val;
+    // 132 % 10 =2
+    // 132/10 =13
+    // 13 % 10 = 3
+    // 13/10 =1
+    // 1%10 = 1
+    // 1/10 =
+    let revNum = 0;
+    while (num > 0) {
+        let val = num % 10;
+        num = Math.floor(num / 10);
+        revNum = revNum * 10 + val;
 
-}
-console.log(revNum)
+    }
 }
 console.log(reverseNum(1234))
+//Constraints
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+ var reverse = function(x) {
+    //123 -- 123 %10 = 3; 123 % 10 =2
+    // 3 *10 +2
+    let reverseNum =0;
+    if(x < -1 * Math.pow(2,31) || x > Math.pow(2,31)-1){return 0};
+    isNegative = x <0 ? true : false;
+    if(isNegative){
+        x = -x;
+    }
+    while(x > 0){
+          reverseNum= reverseNum*10 + x%10;
+          x= Math.floor(x/10); //123--12--1--0
+          
+          }
+if(isNegative){
+        reverseNum= -reverseNum;
+}
+if(reverseNum < -1 * Math.pow(2,31) || reverseNum > Math.pow(2,31)-1){return 0};
+return reverseNum;
+};
+
+console.log(reverse(123))
+//-2pow 31 <= x <= 2pow31 - 1
