bbb7a2725ba30a5395e7822b4f68d406827cc570
sai <saichowdary.7@gmail.com>
2021-09-06 20:58:48 -0400

    new condition in palindrome

--- palyndrome.js
+++ palyndrome.js
@@ -32,3 +32,36 @@ console.log(isPalindrome('abbas'));
     y= String(x).split('').reverse().join('');
     return y === String(x);
 };
+
+/// Third type
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isPalindromeTest = function(s) {
+    
+    //steps
+    //trim spaces, comman, special chars
+    //convert to same case
+    
+    s= s.toLowerCase();
+    
+    s = s.replace(/[^a-zA-Z0-9]+/g,'');
+    s=s.trim();
+    //step2: 
+    let i=0;
+    let j=s.length-1;
+    while(i <= j){
+        if(s[i] !== s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+    
+};
+console.log(isPalindromeTest('A man, a plan, a canal: Panama'))
